c287bd7d446e18e181de4b95f86c7ab6d6b62bb8
Drew Harris <harrisd@smu.edu>
2024-04-25 16:24:22 -0500

    Add solo mode

--- src/BaseHtml.tsx
+++ src/BaseHtml.tsx
@@ -1,14 +1,20 @@
-export const BaseHtml = ({ children }: { children: JSX.Element }) => {
+export const BaseHtml = ({
+  children,
+  solo,
+}: {
+  children: JSX.Element;
+  solo: boolean;
+}) => {
   return (
     <html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script src="https://unpkg.com/tiny-editor/dist/bundle.js"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
       </head>
-      <body hx-ext="preload">{children}</body>
+      <body>{children}</body>
       <script src="https://unpkg.com/tiny-editor/dist/bundle.js"></script>
-      <script src="/script" />
+      {solo ? <script src="/script/solo" /> : <script src="/script" />}
     </html>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -12,15 +12,40 @@ app.get("/", async (c) => {
   const text = await c.env.text.get("bodyText");
   console.log("text", text);
   return c.html(
-    <BaseHtml>
-      <div class="border-black border-2 p-2 m-2 min-h-screen">
-        <div
-          dangerouslySetInnerHTML={{
-            __html: text || "",
-          }}
-          class="min-h-screen border border-red-500"
-          data-tiny-editor
-        ></div>
+    <BaseHtml solo={false}>
+      <div class="p-3">
+        <div>You are in PUBLIC mode.</div>
+        <a class="mb-8 underline" href="/solo">
+          Go Solo
+        </a>
+        <div class="p-2 min-h-screen">
+          <div
+            dangerouslySetInnerHTML={{
+              __html: text || "",
+            }}
+            class="min-h-screen border border-red-500"
+            data-tiny-editor
+          ></div>
+        </div>
+      </div>
+    </BaseHtml>,
+  );
+});
+
+app.get("/solo", async (c) => {
+  return c.html(
+    <BaseHtml solo={true}>
+      <div class="p-3">
+        <div>You are in SOLO mode.</div>
+        <a class="mb-8 underline" href="/">
+          Go Public
+        </a>
+        <div class="p-2 min-h-screen">
+          <div
+            class="min-h-screen border border-red-500"
+            data-tiny-editor
+          ></div>
+        </div>
       </div>
     </BaseHtml>,
   );
@@ -61,6 +86,54 @@ const debouncedUpload = debounce(upload, 200);
 			editor.addEventListener('input', debouncedUpload)
 		)
 	`);
+});
+
+app.get("/script/solo", async (c) => {
+  return c.text(`
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+const localUpload = (e) => {
+console.log(e.target.innerHTML);
+
+window.localStorage.setItem("bodyText", e.target.innerHTML);
+
+}
+
+const debouncedUpload = debounce(localUpload, 200);
+
+	document
+		.querySelectorAll('[data-tiny-editor]')
+		.forEach(editor =>
+			editor.addEventListener('input', debouncedUpload)
+		)
+
+  //load the local storage
+  const bodyText = window.localStorage.getItem("bodyText");
+
+if (bodyText === null) {
+
+  document.querySelector('[data-tiny-editor]').innerHTML = "This is solo mode. Changes are only made locally and are 100% private"
+
+} else {
+
+  document.querySelector('[data-tiny-editor]').innerHTML = bodyText;
+}
+
+
+`);
 });
 
 app.post("/upload", async (c) => {
